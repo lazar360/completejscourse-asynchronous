acfe35ab400f8f769f955eca6ba6b870a034434b
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-08 09:27:29 +0200

    maj script

--- script.js
+++ script.js
@@ -1,6 +1,113 @@
 'use strict';
+// // function + fetch + then response + then data + catch error + finally
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+// };
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>
+    `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  btn.style = 'margin: 10px;';
+  countriesContainer.style.opacity = 1;
+};
+
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+//     return response.json();
+//   });
+// };
+
+// const getCountryData = function (country) {
+//   getJSON(
+//     `https://countries-api-836d.onrender.com/countries/name/${country}`,
+//     'Country not found'
+//   )
+//     // Country 1
+//     .then(data => {
+//       renderCountry(data[0]);
+
+//       const neighbour = data[0].borders?.[0];
+//       if (!neighbour) throw new Error('No neighbour found');
+
+//       // Country 2
+//       return getJSON(
+//         `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+//         'Country not found'
+//       );
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err} 😕`);
+//       renderError(`Something went wrong 😕😕😕 ${err.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', function () {
+//   getCountryData('france');
+// });
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lottery draw is happening 🎲');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You Win 💰');
+//     } else {
+//       reject('You lost your money 😖');
+//     }
+//   }, 2000);
+// });
+// lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// const wait = seconds =>
+//   new Promise(resolve => setTimeout(resolve, seconds * 1000));
+
+// wait(1)
+//   .then(() => {
+//     console.log('I waited for 1 second');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('I waited for 2 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('I waited for 3 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('I waited for 4 seconds');
+//     return wait(1);
+//   });
+
+const whereAmI = async function (country) {
+  const res = await fetch(
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  );
+  const data = await res.json();
+  renderCountry(data[0]);
+};
+whereAmI('portugal');
